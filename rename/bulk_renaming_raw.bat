ce693ca6377b6fe6ba7d1590f7578b01db744eb6
Bishal Tamang <bishal.tamang.dev@gmail.com>
2026-02-02 23:02:50 +0545

    renaming

---
+++ rename/bulk_renaming_raw.bat
@@ -0,0 +1,45 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+rem Ask for common name
+set /p base=Enter common name for all files: 
+
+rem Convert input to lowercase
+set "base=!base:A=a!"
+set "base=!base:B=b!"
+set "base=!base:C=c!"
+set "base=!base:D=d!"
+set "base=!base:E=e!"
+set "base=!base:F=f!"
+set "base=!base:G=g!"
+set "base=!base:H=h!"
+set "base=!base:I=i!"
+set "base=!base:J=j!"
+set "base=!base:K=k!"
+set "base=!base:L=l!"
+set "base=!base:M=m!"
+set "base=!base:N=n!"
+set "base=!base:O=o!"
+set "base=!base:P=p!"
+set "base=!base:Q=q!"
+set "base=!base:R=r!"
+set "base=!base:S=s!"
+set "base=!base:T=t!"
+set "base=!base:U=u!"
+set "base=!base:V=v!"
+set "base=!base:W=w!"
+set "base=!base:X=x!"
+set "base=!base:Y=y!"
+set "base=!base:Z=z!"
+
+set count=1
+
+rem Rename files (skip .bat files)
+for %%F in (*) do (
+    if /i not "%%~xF"==".bat" (
+        ren "%%F" "!base!-!count!%%~xF"
+        set /a count+=1
+    )
+)
+
+exit /b
---
+++ rename/lowercase.bat
@@ -0,0 +1,48 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+rem Rename all folders first
+for /d %%D in (*) do call :rename "%%D"
+
+rem Rename all files
+for %%F in (*) do call :rename "%%F"
+
+exit /b
+
+:rename
+set "old=%~1"
+set "new=%~1"
+
+rem Convert uppercase letters to lowercase
+set "new=!new:A=a!"
+set "new=!new:B=b!"
+set "new=!new:C=c!"
+set "new=!new:D=d!"
+set "new=!new:E=e!"
+set "new=!new:F=f!"
+set "new=!new:G=g!"
+set "new=!new:H=h!"
+set "new=!new:I=i!"
+set "new=!new:J=j!"
+set "new=!new:K=k!"
+set "new=!new:L=l!"
+set "new=!new:M=m!"
+set "new=!new:N=n!"
+set "new=!new:O=o!"
+set "new=!new:P=p!"
+set "new=!new:Q=q!"
+set "new=!new:R=r!"
+set "new=!new:S=s!"
+set "new=!new:T=t!"
+set "new=!new:U=u!"
+set "new=!new:V=v!"
+set "new=!new:W=w!"
+set "new=!new:X=x!"
+set "new=!new:Y=y!"
+set "new=!new:Z=z!"
+
+rem Rename only if the name changed
+if not "%old%"=="%new%" (
+    ren "%old%" "%new%"
+)
+exit /b
